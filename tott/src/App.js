c9b8c9ef59b6cb0b31cbe920e2218cbc78908a53
Austinlp4 <apendergrast44@gmail.com>
2019-03-26 09:28:36 -0500

    finish nav bar

--- tott/src/App.js
+++ tott/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import Home from '../Home/Home.js';
+import Nav from './components/Nav/Nav.js';
+import { Route, withRouter, NavLink } from 'react-router-dom';
+import Home from './components/Home/Home.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <Nav />
+        <Route exact path='/' component={Home}/>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ tott/src/components/Nav/Nav.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+class Nav extends React.Component{
+    render(){
+        return (
+            <NavBar>
+                <h2>Tott</h2>
+                <Links>
+                    <h4>About</h4>
+                    <h4>Blog</h4>
+                    <h4>Merch</h4>
+                    <h4>Contact</h4>
+                </Links>
+            </NavBar>
+        )
+    }
+}
+
+
+const NavBar = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 0 50px 0 30px;
+    width: 100%;
+    background-color: black;
+    color: white;
+`;
+
+const Links = styled.div`
+    display: flex;
+    width: 20%;
+    justify-content: space-between;
+    padding-right: 5%;
+`;
+
+export default Nav;
