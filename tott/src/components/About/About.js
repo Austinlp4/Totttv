d9f6ec242800ce35162e8cd3e7777628f1cfa041
Austinlp4 <apendergrast44@gmail.com>
2019-04-08 14:15:51 -0500

    add content to about

--- tott/src/components/About/About.js
+++ tott/src/components/About/About.js
@@ -5,7 +5,16 @@ class About extends React.Component{
     render(){
         return (
             <div>
-               
+               <div>
+                   <div></div>
+                   <div>
+                       <h1>About Me</h1>
+                       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                           Hic placeat totam dolorum dignissimos, quaerat aperiam ex
+                            minima accusamus debitis, numquam illum aspernatur officia 
+                           tempore modi! Ipsa suscipit impedit rem tempore!</p>
+                   </div>
+               </div>
             </div>
             
         )
